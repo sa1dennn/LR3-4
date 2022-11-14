070d8aec49588ceb260a4b9653bdf8d92bd4c83a
sa1dennn <117730840+sa1dennn@users.noreply.github.com>
2022-11-14 18:55:26 +0300

    Add files via upload

---
+++ for 8.pas
@@ -0,0 +1,15 @@
+﻿var 
+  N: integer;
+  sum: real;
+begin
+  write('Введите количество чисел: ');
+  readln(N);
+  write('Введите числа: ');
+  sum := 0;
+  for var i := 1 to N do
+  begin
+    var x := ReadReal;
+    sum += x;
+  end;
+  writeln('Сумма введенных чисел = ',sum);
+end.
---
+++ for 9.pas
@@ -0,0 +1,8 @@
+﻿var i,n,s:integer;
+begin
+readln(n);
+s:=1;
+for i:=1 to n do
+s:=s*i;
+writeln(s);
+end.
